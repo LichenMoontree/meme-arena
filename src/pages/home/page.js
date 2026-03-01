5b670011b2f67e33a079553a0866963389887e01
Indji <indji@local123.bcserdika.com>
2026-03-01 19:55:32 +0200

    Add env presence check on Home page

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -1,3 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 console.log('Home page loaded');
+console.log('Supabase URL present:', Boolean(import.meta.env.VITE_SUPABASE_URL));
+console.log('Supabase anon key present:', Boolean(import.meta.env.VITE_SUPABASE_ANON_KEY));
